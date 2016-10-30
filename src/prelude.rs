c2aae615cb250696572b3cb8e39c6be47bf8d28c
adjivas <adjivas@users.noreply.github.com>
2016-10-30 09:48:19 +0100

    prelude has now an access on ManagerError

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,7 +2,7 @@ pub use ::{Neko, NekoError};
 pub use ::dynamic::{Compositer, CompositerError};
 pub use ::dynamic::library::{Library, LibraryError};
 
-pub use ::graphic::Manager;
+pub use ::graphic::{Manager, ManagerError};
 pub use ::graphic::sprite::{Sprite, SpriteError};
 pub use ::graphic::sprite::draw::{Draw, DrawError,
                                   SPEC_MAX_XY, SPEC_MAX_X, SPEC_MAX_Y
